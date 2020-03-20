19442cb6b382f6753318f44cc6e4d36fc6a61fcd
fletcher <fletcher@localhost.localdomain>
2020-03-20 15:11:41 -0700

    Updates

---
+++ precache-manifest.489ca53d22b929ef7fef13323a918995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7e468a3dda179e5ea40907e6051bf0",
+    "url": "/todo_app_overkill_edition/index.html"
+  },
+  {
+    "revision": "93e7edd0fb860b7e650b",
+    "url": "/todo_app_overkill_edition/static/css/main.85800983.chunk.css"
+  },
+  {
+    "revision": "8aa8bb107f60afc0ca83",
+    "url": "/todo_app_overkill_edition/static/js/2.ac0c3636.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/todo_app_overkill_edition/static/js/2.ac0c3636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e7edd0fb860b7e650b",
+    "url": "/todo_app_overkill_edition/static/js/main.e04e124c.chunk.js"
+  },
+  {
+    "revision": "c83d1ccd235e7a5b7960",
+    "url": "/todo_app_overkill_edition/static/js/runtime-main.e5c7ea74.js"
+  }
+]);
